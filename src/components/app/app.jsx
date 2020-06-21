6d6acb80df36cd7fac42a0c05c446ab52c4b6567
Maksim Borovik <mouxez@gmail.com>
2020-06-21 21:29:58 +0300

    add /dev-detailed route

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,14 +2,36 @@ import React from 'react';
 import Main from '../main/main';
 import PropTypes from "prop-types";
 import {offerType} from '../../types/offer';
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import OfferDetailed from '../offer-detailed/offer-detailed';
 
 const logoButtonHandler = () => {};
 
-const App = ({offers}) => {
-  return (
-    <Main offers={offers} onLogoButtonClick={logoButtonHandler} />
-  );
-};
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  _renderApp() {
+    const {offers} = this.props;
+    return (
+      <Main offers={offers} onLogoButtonClick={logoButtonHandler} />
+    );
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderApp()}
+          </Route>
+          <Route exact path="/dev-detailed">
+            <OfferDetailed />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   offers: PropTypes.arrayOf(
