7564afb6676ca8e176b4e83a29ae27ed1f543816
Maksim Borovik <mouxez@gmail.com>
2020-07-07 20:31:23 +0300

    refactoring of ReviewsList

--- src/components/reviews-list/reviews-list.jsx
+++ src/components/reviews-list/reviews-list.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReviewsItem from '../reviews-item/reviews-item';
 import PropTypes from "prop-types";
 import {reviewType} from '../../types/review';
+import moment from 'moment';
 
 const ReviewsList = ({reviews}) => {
   const reviewsCount = reviews.length;
@@ -9,9 +9,30 @@ const ReviewsList = ({reviews}) => {
     <>
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviewsCount}</span></h2>
       <ul className="reviews__list">
-        {reviews.length > 0 ? reviews.map((review) => {
+        {reviewsCount > 0 ? reviews.map((review) => {
           return (
-            <ReviewsItem review={review} key={review.id} />
+            <li className="reviews__item" key={review.id}>
+              <div className="reviews__user user">
+                <div className="reviews__avatar-wrapper user__avatar-wrapper">
+                  <img className="reviews__avatar user__avatar" src={review.avatarUrl} width="54" height="54" alt="Reviews avatar" />
+                </div>
+                <span className="reviews__user-name">
+                  {review.user}
+                </span>
+              </div>
+              <div className="reviews__info">
+                <div className="reviews__rating rating">
+                  <div className="reviews__stars rating__stars">
+                    <span style={{width: (Math.floor(review.rating) * 20 + `%`)}}></span>
+                    <span className="visually-hidden">{review.rating}</span>
+                  </div>
+                </div>
+                <p className="reviews__text">
+                  {review.description}
+                </p>
+                <time className="reviews__time" dateTime={moment(review.date).format(`YYYY-MM-DD`)}>{moment(review.date).format(`MMMM YYYY`)}</time>
+              </div>
+            </li>
           );
         }) : null}
       </ul>
