ec5633b4c60ea8c0ac3bb8cc828eb98b4a48942d
Maksim Borovik <mouxez@gmail.com>
2020-07-07 15:10:30 +0300

    change type of component PlacesList to rfc

--- src/components/places-list/places-list.jsx
+++ src/components/places-list/places-list.jsx
@@ -3,30 +3,24 @@ import PropTypes from 'prop-types';
 import PlaceCard from '../place-card/place-card';
 import {offerType} from '../../types/offer';
 
-class PlacesList extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-  }
-  render() {
-    const {offers, onPlaceCardTitleClick, onPlaceCardMouseEnter, isNearPlacesCard} = this.props;
-    return (
-      <div className={isNearPlacesCard ? `near-places__list places__list` : `cities__places-list places__list tabs__content`}>
-        {offers.map((offer) => {
-          return (
-            <PlaceCard
-              key={offer.id}
-              offer={offer}
-              onPlaceCardMouseEnter={onPlaceCardMouseEnter}
-              onPlaceCardTitleClick={onPlaceCardTitleClick}
-              isNearPlacesCard={isNearPlacesCard}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+const PlacesList = ({offers, onPlaceCardTitleClick, onPlaceCardMouseEnter, isNearPlacesCard}) => {
+  const placesClassName = `places__list ${isNearPlacesCard ? `near-places__list` : `cities__places-list tabs__content`}`;
+  return (
+    <div className={placesClassName}>
+      {offers.map((offer) => {
+        return (
+          <PlaceCard
+            key={offer.id}
+            offer={offer}
+            onPlaceCardMouseEnter={onPlaceCardMouseEnter}
+            onPlaceCardTitleClick={onPlaceCardTitleClick}
+            isNearPlacesCard={isNearPlacesCard}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 PlacesList.propTypes = {
   offers: PropTypes.arrayOf(
