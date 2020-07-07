965f562f492f08ae8ae9f298981cf2fdab837b3d
Maksim Borovik <mouxez@gmail.com>
2020-07-07 20:15:30 +0300

    fix render of currentPin

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -5,15 +5,13 @@ import {offerType} from '../../types/offer';
 
 const startCityCoordinates = [52.38333, 4.9];
 const zoom = 12;
-
-const customIcon = Leaflet.icon({
+const pinIcon = Leaflet.icon({
   iconUrl: `img/pin.svg`,
-  iconSize: [30, 30]
+  iconSize: [30, 40]
 });
-
-const currentIcon = Leaflet.icon({
-  iconUrl: `pin-active.svg`,
-  iconSize: [30, 30]
+const activeIcon = Leaflet.icon({
+  iconUrl: `img/pin-active.svg`,
+  iconSize: [30, 40]
 });
 
 class Map extends React.PureComponent {
@@ -23,49 +21,60 @@ class Map extends React.PureComponent {
     this.map = null;
   }
 
-  _renderMapMarkers() {
-    const {offers} = this.props;
-    offers.forEach((offer) => Leaflet.marker(Object.values(offer.location), {customIcon}).addTo(this.map));
-  }
-
   componentDidMount() {
     if (!this.mapRef.current) {
       return;
     }
 
-    this.map = Leaflet.map(this.mapRef.current, {
+    const {offers, currentOffer} = this.props;
+    const mapContainer = this.mapRef.current;
+
+    const map = Leaflet.map(mapContainer, {
+      center: startCityCoordinates,
       zoom,
-      startCityCoordinates,
       zoomControl: false,
       marker: true,
+      layers: [
+        Leaflet.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+          attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+        })]
     });
 
-    this.map.setView(startCityCoordinates, zoom);
-
-    Leaflet.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-    }).addTo(this.map);
-
-    const {currentOffer} = this.props;
     if (currentOffer) {
-      Leaflet.marker(Object.values(currentOffer.location), {currentIcon}).addTo(this.map);
-      this._renderMapMarkers();
+      this._renderMapMarkers(offers, pinIcon, map);
+      Leaflet.marker(Object.values(currentOffer.location), {icon: activeIcon}).addTo(map);
     } else {
-      this._renderMapMarkers();
+      this._renderMapMarkers(offers, pinIcon, map);
     }
+  }
 
+  _renderMapMarkers(cards, icon, map) {
+    cards.forEach((card) => {
+      Leaflet.marker(Object.values(card.location), {icon}).addTo(map);
+    });
+  }
+
+  componentWillUnmount() {
+    const mapContainer = this.mapRef.current;
+    mapContainer.remove();
   }
 
   render() {
-    return <div ref={this.mapRef} style={{width: `100%`, height: `100%`}} />;
+    return (
+      <div
+        ref={this.mapRef}
+        style={{width: `100%`, height: `100%`}}
+      >
+      </div>
+    );
   }
 }
 
 Map.propTypes = {
+  currentOffer: offerType,
   offers: PropTypes.arrayOf(
       offerType.isRequired
   ).isRequired,
-  currentOffer: offerType,
 };
 
 export default Map;
