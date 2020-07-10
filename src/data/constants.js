ebd4252ab8d6dcadf15cbdfad63c262932a01ae2
Maksim Borovik <mouxez@gmail.com>
2020-07-10 16:53:56 +0300

    add more mock data & constants

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,3 +1,11 @@
 const HOUSING_TYPES = [`apartment`, `room`, `house`, `hotel`];
+const MAX_COUNT_CITIES = 6;
+const MAX_COUNT_CLOSEST_OFFERS = 3;
+const MAX_COUNT_REVIEWS = 10;
 
-export {HOUSING_TYPES};
+export {
+  HOUSING_TYPES,
+  MAX_COUNT_CITIES,
+  MAX_COUNT_CLOSEST_OFFERS,
+  MAX_COUNT_REVIEWS,
+};
--- src/mock/offers.js
+++ src/mock/offers.js
@@ -135,6 +135,74 @@ const offers = [
     "title": `Beautiful & luxurious studio at great location`,
     "type": `hotel`
   },
+  {
+    "bedrooms": 5,
+    "city": {
+      "location": {
+        "latitude": 52.370216,
+        "longitude": 4.895168,
+        "zoom": 10
+      },
+      "name": `Berlin`
+    },
+    "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    "goods": [`Heating`, `Kitchen`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+    "host": {
+      "avatarUrl": `http://picsum.photos/90/90?r=5`,
+      "id": 55,
+      "isPro": true,
+      "name": `Anton`
+    },
+    "id": 5,
+    "images": [`http://picsum.photos/260/200?r=1`, `http://picsum.photos/260/200?r=2`, `http://picsum.photos/260/200?r=3`],
+    "isFavorite": false,
+    "isPremium": true,
+    "location": {
+      "latitude": 52.3809553943508,
+      "longitude": 4.939309666406198,
+      "zoom": 8
+    },
+    "maxAdults": 5,
+    "previewImage": `http://picsum.photos/260/200?r=5`,
+    "price": 130,
+    "rating": 2.4,
+    "title": `Beautiful & luxurious studio at great location`,
+    "type": `hotel`
+  },
+  {
+    "bedrooms": 6,
+    "city": {
+      "location": {
+        "latitude": 52.370245,
+        "longitude": 4.895145,
+        "zoom": 10
+      },
+      "name": `Rome`
+    },
+    "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+    "host": {
+      "avatarUrl": `http://picsum.photos/90/90?r=6`,
+      "id": 66,
+      "isPro": false,
+      "name": `Jane`
+    },
+    "id": 6,
+    "images": [`http://picsum.photos/260/200?r=1`, `http://picsum.photos/260/200?r=2`, `http://picsum.photos/260/200?r=3`],
+    "isFavorite": false,
+    "isPremium": true,
+    "location": {
+      "latitude": 52.3809553943508,
+      "longitude": 4.939309666406198,
+      "zoom": 8
+    },
+    "maxAdults": 6,
+    "previewImage": `http://picsum.photos/260/200?r=6`,
+    "price": 160,
+    "rating": 3.5,
+    "title": `Beautiful & luxurious studio at great location`,
+    "type": `hotel`
+  },
 ];
 
 export {offers};
